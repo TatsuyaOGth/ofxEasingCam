42885195e957b96eb960a1cb3629031a176706a7
Tatsuya Ogusu <tatsuyaogusu@gmail.com>
2014-05-18 11:07:34 +0900

    add license

--- src/ofxEasingCam.cpp
+++ src/ofxEasingCam.cpp
@@ -1,82 +1,124 @@
-#include "ofxViewPointRoller.h"
+/**
+ ofxEasingCam.cpp
+ 
+ The MIT License (MIT)
+ 
+ Copyright (c) 2014 Tatsuya Ogusu http://ogsn.org
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
+#include "ofxEasingCam.h"
 
 
-ofxViewPointRoller::ofxViewPointRoller(){
-    scaleN=1.0;
-    scaleT=scaleN;
-    camSpeed=0.1;
+ofxEasingCam::ofxEasingCam()
+{
+    mScaleN = 1.0;
+    mScaleT = mScaleN;
+    mCamSpeed = 0.1;
     
 }
-void ofxViewPointRoller::setup(float width, float height, float _camSpeed,float _scaleance){
-    mWidth = width;
+void ofxEasingCam::setup(float width, float height, float camSpeed,float scaleance)
+{
+    mWidth  = width;
     mHeight = height;
-    scaleN=_scaleance;
-    scaleT=scaleN;
-    camSpeed=ofClamp(_camSpeed,0,1);
+    mScaleN = scaleance;
+    mScaleT = mScaleN;
+    mCamSpeed = ofClamp(camSpeed,0,1);
 }
 
 
-void ofxViewPointRoller::update(){
-    posS.set(posT.x-posN.x,posT.y-posN.y,posT.z-posN.z);
-    posS.set(posS.x*camSpeed,posS.y*camSpeed,posS.z*camSpeed);
-    posN.set(posN.x+posS.x, posN.y+posS.y,posN.z+posS.z);
+void ofxEasingCam::update()
+{
+    mPosS.set(mPosT.x - mPosN.x, mPosT.y - mPosN.y, mPosT.z - mPosN.z);
+    mPosS.set(mPosS.x * mCamSpeed, mPosS.y * mCamSpeed, mPosS.z * mCamSpeed);
+    mPosN.set(mPosN.x + mPosS.x, mPosN.y + mPosS.y, mPosN.z + mPosS.z);
     
-    scaleS=scaleT-scaleN;
-    scaleS=scaleS*camSpeed;
-    scaleN=scaleN+scaleS;
+    mScaleS = mScaleT - mScaleN;
+    mScaleS = mScaleS * mCamSpeed;
+    mScaleN = mScaleN + mScaleS;
 }
 
 
-void ofxViewPointRoller::begin(){
+void ofxEasingCam::begin()
+{
     ofPushMatrix();
     ofTranslate(mWidth * 0.5, mHeight * 0.5);
-    ofRotateX(posN.x);
-    ofRotateY(posN.y);
-    ofRotateZ(posN.z);
-    ofScale(scaleN, scaleN,scaleN);
+    ofRotateX(mPosN.x);
+    ofRotateY(mPosN.y);
+    ofRotateZ(mPosN.z);
+    ofScale(mScaleN, mScaleN, mScaleN);
 }
 
-void ofxViewPointRoller::begin(float _w, float _h){
-    ofPushMatrix();
-    ofTranslate(_w * 0.5, _h * 0.5);
-    ofRotateX(posN.x);
-    ofRotateY(posN.y);
-    ofRotateZ(posN.z);
-    ofScale(scaleN, scaleN,scaleN);
+void ofxEasingCam::begin(float w, float h)
+{
+    mWidth = w;
+    mHeight = h;
+    begin();
 }
 
-
-void ofxViewPointRoller::end(){
+void ofxEasingCam::end(){
     ofPopMatrix();
     
 }
 
-
-void ofxViewPointRoller::setCamSpeed(float _camSpeed){
-    camSpeed=ofClamp(_camSpeed,0,1);
+void ofxEasingCam::setWindowSize(float width, float height)
+{
+    mWidth = width;
+    mHeight = height;
 }
 
-void ofxViewPointRoller::setRandomPos(float _randomAngle){
+
+void ofxEasingCam::setCamSpeed(float camSpeed)
+{
+    camSpeed = ofClamp(mCamSpeed, 0, 1);
+}
+
+void ofxEasingCam::setRandomPos(float randomAngle)
+{
     ofVec3f ram3f;
-    int ramSize=_randomAngle;
-    ram3f.set(ofRandom(-ramSize, ramSize),ofRandom(-ramSize, ramSize),ofRandom(-ramSize, ramSize));
-    posT.set(ofRandom(posN.x,(int)(posN.x+ram3f.x)%360), ofRandom(posN.y,(int)(posN.y+ram3f.y)%360),ofRandom(posN.z,(int)(posN.z+ram3f.z)%360));
+    int ramSize = randomAngle;
+    ram3f.set(ofRandom(-ramSize, ramSize),
+              ofRandom(-ramSize, ramSize),
+              ofRandom(-ramSize, ramSize));
+    mPosT.set(ofRandom(mPosN.x,(int)(mPosN.x + ram3f.x) % 360),
+              ofRandom(mPosN.y,(int)(mPosN.y + ram3f.y) % 360),
+              ofRandom(mPosN.z,(int)(mPosN.z + ram3f.z) % 360));
 }
 
-void ofxViewPointRoller::setPos(float _x,float _y,float _z){
-    posT.set(_x,_y,_z);
+void ofxEasingCam::setPos(float x,float y,float z)
+{
+    mPosT.set(x, y, z);
 }
 
-void ofxViewPointRoller::setPos(ofVec3f & pos){
-    posT.set(pos.x, pos.y, pos.z);
+void ofxEasingCam::setPos(ofVec3f & pos)
+{
+    mPosT.set(pos.x, pos.y, pos.z);
 }
 
-void ofxViewPointRoller::setRandomScale(float _min,float _max){
-    scaleT=ofRandom(_min,_max);
+void ofxEasingCam::setRandomScale(float min,float max)
+{
+    mScaleT = ofRandom(min, max);
 }
 
-void ofxViewPointRoller::setScale(float _scale){
-    scaleT=_scale;
+void ofxEasingCam::setScale(float scale)
+{
+    mScaleT = scale;
 }
 
 
--- src/ofxEasingCam.h
+++ src/ofxEasingCam.h
@@ -1,29 +1,68 @@
+/**
+ ofxEasingCam.h
+ 
+ The MIT License (MIT)
+
+ Copyright (c) 2014 Tatsuya Ogusu http://ogsn.org
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
 #pragma once
 
 #include "ofMain.h"
 
-class ofxViewPointRoller
+class ofxEasingCam
 {
 public:
-    ofxViewPointRoller();
-    void setup(float width = ofGetWidth(), float height = ofGetHeight(), float _camSpeed=0.1, float _distance=1.0);
+    ofxEasingCam();
+    void setup(float width      = ofGetWidth(),
+               float height     = ofGetHeight(),
+               float camSpeed   = 0.1,
+               float distance   = 1.0);
+    
     void update();
+    
     void begin();
-    void begin(float _w, float _h);
+    void begin(float w, float h);
     void end();
-    void setCamSpeed(float _camSpeed);
-    void setRandomPos(float _randomAngle=180);
-    void setPos(float _x,float _y,float _z);
+    
+    void setWindowSize(float width, float height);
+    void setCamSpeed(float camSpeed);
+    void setRandomPos(float randomAngle = 180);
+    void setPos(float x, float y, float z);
     void setPos(ofVec3f & pos);
-    void setRandomScale(float _min,float _max);
-    void setScale(float _dist);
+    void setRandomScale(float min, float max);
+    void setScale(float dist);
+    
+    ofVec3f getPos(){ return mPosN; }
+    ofVec3f getTargetPos(){ return mPosT; }
+    float getScale(){ return mScaleN; }
 
-    
-    ofVec3f posN,posT,posS;
-    ofLight lig;
-    float camSpeed;
-    float scaleN,scaleT,scaleS;
-    
+private:
+    ofVec3f mPosN;
+    ofVec3f mPosT;
+    ofVec3f mPosS;
+    float mCamSpeed;
+    float mScaleN;
+    float mScaleT;
+    float mScaleS;
+
     float mWidth;
     float mHeight;
 };
